212fcf6eb5b21189523e20d4c027a1bb06b47181
Dominicode-s <60432849+Dominicode-s@users.noreply.github.com>
2020-03-02 18:18:12 +0000

    Added loop for the amount of pages

--- testcode.py
+++ testcode.py
@@ -10,35 +10,41 @@ whilecounter = 1
 url = "https://www.indeed.co.uk/jobs?q=&l=bb10&radius=10"
 page = "&start="
 
-html_content = requests.get(url).text
 
-soup = BeautifulSoup(html_content, 'lxml')
-
-for joblisting in soup.find_all('div', class_='jobsearch-SerpJobCard'):
-    jobtitle = joblisting.div.a.text
-    companyName = joblisting.span.text
-
-    money1 = joblisting.find('div', class_='salarySnippet')
-    if str(money1) != 'None':
-        money1 = money1.span.text
-
-    summary = joblisting.find('div', class_='summary').text
-    footer = joblisting.find('div', class_='footer')
-
-    if str(footer) != 'None':
-        footer = footer.text
+while whilecounter < howmanypages:
+    whilecounter += 1
+    if whilecounter > 1:
+        url = url + page + str(counter)
+        counter += 10
+        
+    html_content = requests.get(url).text
 
 
-    print('Title: ', jobtitle)
-    print()
-    print('Company Name: ',companyName)
-    print()
-    print('Salary: ',money1)
-    print()
-    print('Summary: ',summary)
-    print()
-    print('Footer: ',footer)
+    soup = BeautifulSoup(html_content, 'lxml')
 
-    print('_____________________________')
+    for joblisting in soup.find_all('div', class_='jobsearch-SerpJobCard'):
+        jobtitle = joblisting.div.a.text
+        companyName = joblisting.span.text
+
+        money1 = joblisting.find('div', class_='salarySnippet')
+        if str(money1) != 'None':
+            money1 = money1.span.text
+
+        summary = joblisting.find('div', class_='summary').text
+        footer = joblisting.find('div', class_='footer')
+
+        if str(footer) != 'None':
+            footer = footer.text
 
 
+        print('Title: ', jobtitle)
+        print()
+        print('Company Name: ',companyName)
+        print()
+        print('Salary: ',money1)
+        print()
+        print('Summary: ',summary)
+        print()
+        print('Footer: ',footer)
+
+        print('_____________________________')
